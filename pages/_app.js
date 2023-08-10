68c1a8f542dd7b2355b78069b543b75ac90dcba7
TyamackovM <tyamackov@icloud.com>
2023-08-10 13:45:20 +0300

    transition animate frame

--- pages/_app.js
+++ pages/_app.js
@@ -3,17 +3,24 @@ import '../styles/globals.css';
 //? router
 import { useRouter } from 'next/router';
 
+//? framer motion
+import { AnimatePresence, motion } from 'framer-motion';
+
 //? components
 import Layout from '../components/Layout';
+import Transition from '../components/Transition';
 
 export default function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
   return (
-    <div>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </div>
-  )
+    <Layout>
+      <AnimatePresence mode='wait'>
+        <motion.div key={router.route} className='h-full'>
+          <Transition />
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
+    </Layout>
+  );
 }
