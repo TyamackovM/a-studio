39e58958cf4d880db61709291c18d1efbdab141d
Aleksei Vlasov <aleks101989@mail.ru>
2023-08-15 20:08:34 +0800

    chat added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css';
 import { AppProps } from 'next/app';
+import TawkMessengerReact from '@tawk.to/tawk-messenger-react';
 
 //? router
 import { useRouter } from 'next/router';
@@ -16,9 +17,14 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
 
   return (
     <Layout>
+   
       <AnimatePresence mode='wait'>
         <motion.div key={router.route} className='h-full'>
+    
           <Transition />
+          <TawkMessengerReact
+                propertyId="64d8b031cc26a871b02ef4a4"
+                widgetId="1h7sd5pgl"/>
           <Component {...pageProps} />
         </motion.div>
       </AnimatePresence>
