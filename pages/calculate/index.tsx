e9fdbf5c7ba89889c2841af4447a592387618890
Aleksei Vlasov <aleks101989@mail.ru>
2023-08-19 18:53:12 +0800

    test

--- pages/calculate/index.tsx
+++ pages/calculate/index.tsx
@@ -28,7 +28,7 @@ export default function index() {
         </div>
         <div className="bg-[#ff6219] w-screen md:w-2/6 md:h-full flex flex-col items-center justify-evenly md:p-8">
           <span className="mt-[10px] md:mt-0">Объект:</span>
-          <select className="w-[140px] md:w-1/2 text-white-500 text-center bg-transparent text-[16px] text-4xl xl:text-3xl lg:text-2xl md:text-xl sm:text-l text-center border-b-2 border-c66900 border-t-0 border-l-0 border-r-0 border-rounded-0 outline-none resize-none whitespace-normal overflow-ellipsisalign-middle whitespace-nowrap select-auto cursor-pointer my-0 border-1 border-solid border-white-500">
+          <select className="w-[140px] md:w-1/2 text-white-500 bg-transparent text-[16px] text-4xl xl:text-3xl lg:text-2xl md:text-xl sm:text-l text-center border-b-2 border-c66900 border-t-0 border-l-0 border-r-0 border-rounded-0 outline-none resize-none whitespace-normal overflow-ellipsisalign-middle whitespace-nowrap select-auto cursor-pointer my-0 border-1 border-solid border-white-500">
             <option value="option1">Выберите...</option>
             <option value="option2">Комната</option>
             <option value="option3">1-к квартира</option>
@@ -62,7 +62,7 @@ export default function index() {
       <div className="md:w-4/5 grid grid-cols-2 md:grid-cols-3 grid-rows-2 align-center gap-[30px] mt-[10px] md:mt-[30px]">
         <div className="flex flex-col items-center md:justify-center">
           <div className="text-[60px] md:text-[80px] text-accent mb-4">
-            <CiDesktop className="iconSmall"/>
+            <CiDesktop className="iconSmall" />
           </div>
           <span className="text-center text-[14px] md:text-[16px]">
             Проектирование 1500 ₽/㎡
@@ -70,7 +70,7 @@ export default function index() {
         </div>
         <div className="flex flex-col items-center md:justify-center">
           <div className="text-[60px] md:text-[80px] text-accent mb-4">
-            <CiHome className="iconSmall"/>
+            <CiHome className="iconSmall" />
           </div>
           <span className="text-center text-[14px] md:text-[16px]">
             Визуализация дизайн проекта 1500 ₽/㎡
@@ -78,7 +78,7 @@ export default function index() {
         </div>
         <div className="flex flex-col items-center md:justify-center">
           <div className="text-[60px] md:text-[80px] text-accent mb-4">
-            <CiViewTimeline className="iconSmall"/>
+            <CiViewTimeline className="iconSmall" />
           </div>
           <span className="text-center text-[14px] md:text-[16px]">
             Эскизная часть проекта 1500 ₽/㎡
@@ -86,7 +86,7 @@ export default function index() {
         </div>
         <div className="flex flex-col items-center md:justify-center">
           <div className="text-[60px] md:text-[80px] text-accent mb-4">
-            <CiUnlock className="iconSmall"/>
+            <CiUnlock className="iconSmall" />
           </div>
           <span className="text-center text-[14px] md:text-[16px]">
             Дизай проект под ключ 1500 ₽/㎡
@@ -94,7 +94,7 @@ export default function index() {
         </div>
         <div className="flex flex-col items-center md:justify-center">
           <div className="text-[60px] md:text-[80px] text-accent mb-4">
-            <CiCrop className="iconSmall"/>
+            <CiCrop className="iconSmall" />
           </div>
           <span className="text-center text-[14px] md:text-[16px]">
             Обмерочный чертеж 100 ₽/㎡
@@ -102,7 +102,7 @@ export default function index() {
         </div>
         <div className="flex flex-col items-center md:justify-center">
           <div className="text-[60px] md:text-[80px] text-accent mb-4">
-            <CiMountain1 className="iconSmall"/>
+            <CiMountain1 className="iconSmall" />
           </div>
           <span className="text-center text-[14px] md:text-[16px]">
             Авторский надзор 50000 ₽/мес
