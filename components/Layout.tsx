8095046895c975e4efdb7e0e1c31cf7855cdfb02
Aleksei Vlasov <aleks101989@mail.ru>
2023-09-30 18:32:48 +0800

    scroll fixed

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,40 +1,42 @@
-import React, { ReactNode}  from 'react';
-import Header from './Header';
+import React, { ReactNode } from "react";
+import Header from "./Header";
 
 //* fonts
-import {Sora} from '@next/font/google'
-import { Playfair_Display, Jost} from '@next/font/google';
+import { Sora } from "@next/font/google";
+import { Playfair_Display, Jost } from "@next/font/google";
 
 //*font settings
 const sora = Sora({
-  subsets: ['latin'],
-  variable: '--font-sora',
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
-})
+  subsets: ["latin"],
+  variable: "--font-sora",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+});
 
 const playfair = Playfair_Display({
-  subsets: ['cyrillic'],
-  weight: ['600', '700', '800'],
-  style: 'italic'
-})
+  subsets: ["cyrillic"],
+  weight: ["600", "700", "800"],
+  style: "italic",
+});
 
 const jost = Jost({
-  style: 'italic',
-  subsets: ['cyrillic'],
-  weight: ['500', '600', '700', '800']
-})
+  style: "italic",
+  subsets: ["cyrillic"],
+  weight: ["500", "600", "700", "800"],
+});
 
 type LayoutProps = {
   children: ReactNode;
-}
+};
 
-const Layout: React.FC <LayoutProps> = ({children}) => {
+const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className={`page text-white bg-cover bg-no-repeat ${jost.className} font-sora relative`}>
+    <div
+      className={`page text-white bg-cover bg-no-repeat ${jost.className} font-sora relative`}
+    >
       <Header />
       {children}
     </div>
-  )
-}
+  );
+};
 
 export default Layout;
