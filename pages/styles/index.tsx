929b467eb0f77029b395ecae59b04ebb3e145798
Aleksei Vlasov <aleks101989@mail.ru>
2023-09-30 18:48:16 +0800

    styles scroll fixed

--- pages/styles/index.tsx
+++ pages/styles/index.tsx
@@ -2,7 +2,7 @@ import ServiceSlider from "../../components/ServiceSlider";
 
 export default function index() {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex items-center justify-center">
       <div className="container mx-auto">
         <ServiceSlider />
       </div>
